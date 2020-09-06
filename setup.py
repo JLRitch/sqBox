810b4dd23789a57621ecc5cf0343ce83856d636d
Jere <jereritchie@gmail.com>
2020-09-05 20:13:25 -0400

    Added dist

--- setup.py
+++ setup.py
@@ -23,5 +23,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT",
         "Operating System :: OS Independent"
     ],
-    python_requires=">=3.6"
+    python_requires=">=3.6",
+    license="MIT"
 )
